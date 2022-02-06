712f0082511a62139b08225bf67393f1607648bf
david-pekar <davidpekar@utexas.edu>
2022-02-05 18:40:51 -0600

    commit on main

---
+++ website4.js
@@ -0,0 +1,13 @@
+let btnSend = document.querySelector('#btnSend');
+btnSend.addEventListener('click', () => btnSend.style.backgroundColor='#337ab7');
+
+
+document.querySelector('#btnSend').addEventListener('click', function() {
+    alert('Some JavaScript');
+  });
+
+  function multiply(num1,num2) {
+    let result = num1 * num2;
+    return result;
+  }
+
